6bc90c7fab664b9e21fbf977a92a15518602f700
Michael Wiles <michael.wiles@afrozaar.com>
2015-05-08 11:57:57 +0200

    updating to use latest of various things

--- eclipselink-bug/src/main/java/model/ApplicationContext.java
+++ eclipselink-bug/src/main/java/model/ApplicationContext.java
@@ -3,11 +3,14 @@ package model;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import com.jolbox.bonecp.BoneCPDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import org.eclipse.persistence.jpa.PersistenceProvider;
 
@@ -20,7 +23,8 @@ import java.util.Properties;
 @ComponentScan(basePackages = { "model" })
 public class ApplicationContext {
 
-    @Bean
+    @Bean(name = "dataSource")
+    @Profile("hsqldb")
     public DataSource dataSource() {
         BoneCPDataSource dataSource = new BoneCPDataSource();
         dataSource.setDriverClass("org.hsqldb.jdbc.JDBCDriver");
@@ -29,6 +33,50 @@ public class ApplicationContext {
         dataSource.setPassword("");
         dataSource.setLogStatementsEnabled(true);
         return dataSource;
+    }
+
+    @Profile("x")
+    public DataSource MysqlDataSource() {
+        BoneCPDataSource dataSource = new BoneCPDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/ashes?autoReconnect=true&amp;sessionVariables=storage_engine=InnoDB;");
+
+        dataSource.setUsername("ashes");
+        dataSource.setPassword("ashes");
+
+        dataSource.setMaxConnectionsPerPartition(1);
+        dataSource.setPartitionCount(1);
+        dataSource.setMinConnectionsPerPartition(1);
+        dataSource.setLogStatementsEnabled(true);
+        return dataSource;
+    }
+
+    /**
+     * <bean id="mysqlHikariDataSource"
+     * class="com.zaxxer.hikari.HikariDataSource"> <constructor-arg> <bean
+     * class="com.zaxxer.hikari.HikariConfig"> <property name="driverClassName"
+     * value="com.mysql.jdbc.Driver" /> <property name="username"
+     * value="${db.username}" /> <property name="password"
+     * value="${db.password}" /> <property name="jdbcUrl" value="${db.url}" />
+     * <property name="maximumPoolSize" value="25" /> <property name="poolName"
+     * value="${poolName}" /> <property name="transactionIsolation"
+     * value="${transactionIsolationLevelString}" /> </bean> </constructor-arg>
+     * </bean>
+     */
+    @Bean(name = "dataSource")
+    @Profile("mysql")
+    public DataSource HikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/ashes?autoReconnect=true&amp;sessionVariables=storage_engine=InnoDB;");
+
+        config.setUsername("root");
+        config.setPassword("*#idoJ0=-");
+
+        config.setMaximumPoolSize(2);
+        config.setMinimumIdle(1);
+
+        return new HikariDataSource(config);
     }
 
     @Bean
@@ -51,7 +99,8 @@ public class ApplicationContext {
         jpaProperties.put("eclipselink.ddl-generation", "create-tables");
         jpaProperties.put("eclipselink.weaving", "static");
         jpaProperties.put("eclipselink.ddl-generation.output-mode", "database");
-        jpaProperties.put("eclipselink.target-database", "HSQL");
+        //jpaProperties.put("eclipselink.target-database", "HSQL");
+        jpaProperties.put("eclipselink.target-database", "MYSQL");
         entityManagerFactoryBean.setJpaProperties(jpaProperties);
 
         return entityManagerFactoryBean;
--- eclipselink-bug/src/test/java/model/SimpleTest.java
+++ eclipselink-bug/src/test/java/model/SimpleTest.java
@@ -3,8 +3,10 @@ package model;
 import com.google.common.collect.Lists;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,7 @@ import javax.persistence.PersistenceContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationContext.class)
+@ActiveProfiles("mysql")
 public class SimpleTest {
 
     @Autowired
@@ -23,6 +26,7 @@ public class SimpleTest {
     EntityManager em;
 
     @Test
+    @Transactional
     public void TestLookupWithMoreOneEnumInTheFilter() {
         System.out.println(repository.findAll());
         Surname surname = new Surname();
@@ -32,6 +36,7 @@ public class SimpleTest {
     }
 
     @Test
+    @Transactional
     public void TestLookupWithMoreThanOneEnumInTheFilter() {
         System.out.println(repository.findAll());
         Surname surname = new Surname();
